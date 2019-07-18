1e2efc8ad7ce6eb9224cae28a7d210a845228925
Phanuphat Cholatda <phanuphat.cho@gmail.com>
2019-07-19 00:53:59 +0700

    grid can initial space

--- src/game-of-life.test.ts
+++ src/game-of-life.test.ts
@@ -1,10 +1,16 @@
-import {nextLife} from './game-of-life';
+import {nextStateCell, initGridSpace} from './game-of-life';
 
-describe('Cell ', () => {
+describe('Cells', () => {
     describe('Any live cell with fewer than two live neighbours dies, as if by underpopulation.', () => {
-        it('Should be die(false) when neighbours less than 2', function () {
+        it('Should be die(false) when neighbours is 1', function () {
             const neighbours: boolean[] = [true, false, false, false, false, false, false, false]
-            const result: boolean = nextLife(neighbours)
+            const result: boolean = nextStateCell(neighbours)
+            expect(result).toBeFalsy()
+        })
+
+        it('Should be die(false) when no neighbours', function () {
+            const neighbours: boolean[] = [false, false, false, false, false, false, false, false]
+            const result: boolean = nextStateCell(neighbours)
             expect(result).toBeFalsy()
         });
     })
@@ -12,13 +18,13 @@ describe('Cell ', () => {
     describe('Any live cell with two or three live neighbours lives on to the next generation.', () => {
         it('Should be alive(true) when neighbours is 2', () => {
             const neighbours: boolean[] = [true, true, false, false, false, false, false, false]
-            const result: boolean = nextLife(neighbours)
+            const result: boolean = nextStateCell(neighbours)
             expect(result).toBeTruthy()
         })
 
         it('Should be alive(true) when neighbours is 3', () => {
             const neighbours: boolean[] = [true, true, false, false, false, false, false, true]
-            const result: boolean = nextLife(neighbours)
+            const result: boolean = nextStateCell(neighbours)
             expect(result).toBeTruthy()
         })
     })
@@ -26,7 +32,7 @@ describe('Cell ', () => {
     describe('Any live cell with more than three live neighbours dies, as if by overpopulation', () => {
         it('Should be die(false) when neighbours over 3', () => {
             const neighbours: boolean[] = [true, true, true, false, false, false, false, true]
-            const result: boolean = nextLife(neighbours)
+            const result: boolean = nextStateCell(neighbours)
             expect(result).toBeFalsy()
         })
     })
@@ -34,14 +40,34 @@ describe('Cell ', () => {
     describe('Any dead cell with three live neighbours becomes a live cell, as if by reproduction.', () => {
         it('Should be reborn when neighbours is 3', () => {
             const neighbours: boolean[] = [true, true, false, false, false, false, false, true]
-            const result: boolean = nextLife(neighbours, false)
+            const result: boolean = nextStateCell(neighbours, false)
             expect(result).toBeTruthy()
         })
 
         it('Should be reborn when neighbours more than 3', () => {
             const neighbours: boolean[] = [true, true, true, false, false, false, false, true]
-            const result: boolean = nextLife(neighbours, false)
+            const result: boolean = nextStateCell(neighbours, false)
             expect(result).toBeTruthy()
+        })
+    })
+})
+
+describe('The Grid', () => {
+    describe('grid can initial space', () => {
+        const width: number = 100
+        const height: number = 100
+        const grid: boolean[][] = initGridSpace(width, height)
+
+        it('gird size should be width * height ', function () {
+            const gridSize: number = grid.reduce((size, rows) => rows.length, 0)
+
+            expect(gridSize).toBe(width * height)
+        })
+
+        it('every cells on new grid is dead by default.', function () {
+            const isAllDead: boolean = grid.reduce((isDead, rows) => rows.every(row => row == false), true)
+            
+            expect(isAllDead).toBeTruthy
         })
     })
 })
--- src/game-of-life.ts
+++ src/game-of-life.ts
@@ -1,4 +1,19 @@
-export const nextLife = (neighbours: boolean[], isAlive: boolean = true,) => {
+export const initGridSpace = (width: number, height: number) => {
+    let grid: boolean[][] = []
+    let row: boolean[] = []
+
+    for (let w = 0; w < width; w++) {
+        for (let h = 0; h < height; h++) {
+            row.push(false)
+        }
+        grid.push(row)
+    }
+
+    return grid
+}
+
+
+export const nextStateCell = (neighbours: boolean[], isAlive: boolean = true,) => {
     const count = neighbours.filter((neighbourIsAlive) => neighbourIsAlive).length
 
     if (isAlive) {
