818c512b4b7a63a551ff57e10bdc648254c34c81
Phanuphat Cholatda <phanuphat.cho@gmail.com>
2019-07-18 22:50:53 +0700

    add cell behavior

--- src/game-of-life.test.ts
+++ src/game-of-life.test.ts
@@ -1,6 +1,48 @@
-import { GameOfLife } from './game-of-life';
+import {nextLife} from './game-of-life';
 
-test('GameOfLife is instantiated', () => {
-  const gameOfLife = new GameOfLife();
-  expect(gameOfLife).toBeInstanceOf(GameOfLife);
-});
+describe('Cell ', () => {
+    describe('Any live cell with fewer than two live neighbours dies, as if by underpopulation.', () => {
+        it('Should be die(false) when neighbours less than 2', function () {
+            const neighbours: boolean[] = [true, false, false, false, false, false, false, false]
+            const result: boolean = nextLife(neighbours)
+            expect(result).toBeFalsy()
+        });
+    })
+
+    describe('Any live cell with two or three live neighbours lives on to the next generation.', () => {
+        it('Should be alive(true) when neighbours is 2', () => {
+            const neighbours: boolean[] = [true, true, false, false, false, false, false, false]
+            const result: boolean = nextLife(neighbours)
+            expect(result).toBeTruthy()
+        })
+
+        it('Should be alive(true) when neighbours is 3', () => {
+            const neighbours: boolean[] = [true, true, false, false, false, false, false, true]
+            const result: boolean = nextLife(neighbours)
+            expect(result).toBeTruthy()
+        })
+    })
+
+    describe('Any live cell with more than three live neighbours dies, as if by overpopulation', () => {
+        it('Should be die(false) when neighbours over 3', () => {
+            const neighbours: boolean[] = [true, true, true, false, false, false, false, true]
+            const result: boolean = nextLife(neighbours)
+            expect(result).toBeFalsy()
+        })
+    })
+
+    describe('Any dead cell with three live neighbours becomes a live cell, as if by reproduction.', () => {
+        it('Should be reborn when neighbours is 3', () => {
+            const neighbours: boolean[] = [true, true, false, false, false, false, false, true]
+            const result: boolean = nextLife(neighbours, false)
+            expect(result).toBeTruthy()
+        })
+
+        it('Should be reborn when neighbours more than 3', () => {
+            const neighbours: boolean[] = [true, true, true, false, false, false, false, true]
+            const result: boolean = nextLife(neighbours, false)
+            expect(result).toBeTruthy()
+        })
+    })
+})
+
--- src/game-of-life.ts
+++ src/game-of-life.ts
@@ -1,1 +1,9 @@
-export class GameOfLife {}
+export const nextLife = (neighbours: boolean[], isAlive: boolean = true,) => {
+    const count = neighbours.filter((neighbourIsAlive) => neighbourIsAlive).length
+
+    if (isAlive) {
+        return count === 2 || count === 3
+    } else {
+        return count >= 3
+    }
+}
